dd3f4844fc8b34fe108eb589b435446119ee6893
nicholas pellizer <nicholas.pellizer@gmail.com>
2023-12-21 17:20:29 +0100

    update configuration for portainer

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 export RESTIC_PASSWORD=restic
 restic -r /home/jigen/backup/restic-homelab --verbose backup /home/vol-docker
+restic -r /home/jigen/backup//restic-homelab forget --keep-last 12 --prune
